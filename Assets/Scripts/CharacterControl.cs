fd7f9875439239e3615d20f026d7088e42a0d02d
tyrosine1153 <tyrosine1153@gmail.com>
2021-12-05 22:15:05 +0900

    Edit scripts to character control System [미완]
    - ToDo 표시함

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,25 +1,33 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 
 public partial class CharacterControl : MonoBehaviour
 {
-    private State<CharacterControl> CharacterState = new IdleState();
+    private readonly State<CharacterControl>[] states = {new IdleState(), new SkillState(), new WalkState()};
+    private State<CharacterControl> _curState;
+
+    // Todo : 플레이어 캐릭터 기본 세팅(상태0, 히트박스0, 이동)
 
     private void Start()
     {
+        _curState = states[(int)State.Idle];
+        
         this.UpdateAsObservable().Subscribe(_ =>
         {
-            CharacterState.Action(this);
+            _curState.Action(this);
+            var nextState = _curState.InputHandle(this);
             
-            var nextState = CharacterState.InputHandle(this);
-            if (!CharacterState.Equals(nextState))
+            if (!_curState.Equals(nextState))
             {
-                CharacterState = nextState;
+                _curState.Exit(this);
+                _curState = nextState;
+                _curState.Enter(this);
             }
         }).AddTo(this);
     }
-
+    
     // DamageCollider
     public void OnCollisionEnterInChildren(Collision2D other)
     {
