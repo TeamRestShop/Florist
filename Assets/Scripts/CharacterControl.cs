88c3e85af7f2784719694634d59d984c88d822fd
tyrosine1153 <tyrosine1153@gmail.com>
2021-12-05 16:44:44 +0900

    Add Scripts to state system
    플레이어 상태 정의

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public partial class CharacterControl : MonoBehaviour
+{
+    private State<CharacterControl> CharacterState = new IdleState();
+
+    private void Start()
+    {
+        this.UpdateAsObservable().Subscribe(_ =>
+        {
+            CharacterState.Action(this);
+            
+            var nextState = CharacterState.InputHandle(this);
+            if (!CharacterState.Equals(nextState))
+            {
+                CharacterState = nextState;
+            }
+        }).AddTo(this);
+    }
+    
+    
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public abstract class State<T>
+{
+    public Action<T> Action;
+    public abstract State<T> InputHandle(T t);
+
+    public State()
+    {
+        Action = Enter;
+    }
+
+    public virtual void Enter(T t)
+    {
+        Action = Update;
+    }
+
+    public virtual void Update(T t)
+    {
+        
+    }
+
+    public virtual void Exit(T t)
+    {
+        
+    }
+}
