f504fe3d7c0daa285e46b78ba17c4db7311281b3
tyrosine1153 <tyrosine1153@gmail.com>
2021-12-05 16:43:39 +0900

    Edit Script to fix typo

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using UniRx;
-using UniRx.Triggers;
+﻿using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class JoystickControl : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    public readonly Subject<Vector2> joystickDirection = new Subject<Vector2>();
+    public readonly BoolReactiveProperty isJoystickDown = new BoolReactiveProperty();
+    
+    // Components
     private RectTransform _thisTransform;  // 현재 조이스틱
     private RectTransform _pointTransform;  // 조이스틱 손잡이
     
-    private Vector2 _pointPosition;  // point 원래 자리
-    private float _pointRadius;  // point 반지름
-    private float _posToDir;  // 입력 위치 > 입력 방향으로 변환할때 사용하는 값 (1 / _pointRange)
-
+    // Constants
     private const float MinRange = 0.3f;  // 입력 반지름 최소 범위
     private const float MaxRange = 1.3f;  // 입력 반지름 최대 범위
-    
-    public readonly Subject<Vector2> joystickDirection = new Subject<Vector2>();
-    public readonly BoolReactiveProperty isJoystickDown = new BoolReactiveProperty();
+
+    private Vector2 _pointPosition;  // point 기본 자리
+    private float _pointRadius;  // point 반지름
+    private float _posToDir;  // 입력 위치 > 입력 방향으로 변환할때 사용하는 값 (1 / _pointRadius)
 
     public void OnDrag(PointerEventData eventData)
     {
