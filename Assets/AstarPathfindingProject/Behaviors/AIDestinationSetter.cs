cc2099cb1592f430b5ab9be4759262c293443745
YellowHowler <rittachoi1@gmail.com>
2022-02-15 21:57:39 +0900

    ADD Level Select UI
    -

--- Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -15,16 +15,10 @@ namespace Pathfinding {
 	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
 	public class AIDestinationSetter : VersionedMonoBehaviour {
 		/// <summary>The object that the AI should move to</summary>
-		public Transform target{get;set;}
+		public Transform target;
 		IAstarAI ai;
 
-		void Awake()
-		{
-			target = GameObject.FindWithTag("GoalObject").GetComponent<Transform>();
-		}
-
 		void OnEnable () {
-			
 			ai = GetComponent<IAstarAI>();
 			// Update the destination right before searching for a path as well.
 			// This is enough in theory, but this script will also update the destination every
